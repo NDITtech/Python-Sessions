f2934963eb7c3447f1fd92b2771d38b20307e68b
vijay740839 <vijay740839@gmail.com>
2025-10-18 21:44:41 +0530

    all files added

---
+++ Introduction/Intro_file.py
@@ -0,0 +1,114 @@
+#x = 10
+
+# this is the variable and assigning the value 10
+
+
+'''
+x = 10
+print(x)
+'''
+
+
+"""
+x = 5
+print(x)
+"""
+
+
+'''
+
+x = 3
+y = 5
+z = 6
+
+x = "apple"
+
+
+
+
+
+print(x)
+print(y)
+
+
+
+#res = lambda a, b: a + b
+#print(res(10, 5))
+
+'''
+
+
+
+#Indentation
+
+if 2>1:
+    print("correct")
+    print("THIS IS CORRECT")
+
+
+a = [1, 2, 3]
+b = a         # both point to the same list
+b.append(4)
+print(a)      # Output: [1, 2, 3, 4]
+
+
+
+x = 10
+#print(x)
+x = "hello"   # Now x refers to a string
+print(x)
+
+
+a, b, c = 1, 2, 3      # multiple variables at once
+x = y = z = 0          # same value to multiple variables
+
+print(a)
+print(b)
+print(c)
+
+print(x)
+print(y)
+print(z)
+
+
+
+x, y, z = "how", "are", "you"
+print(x)
+print(y)
+print(z)
+x = y = z = "hi"
+print(x)
+print(y)
+print(z)
+
+
+
+x = "are"
+y = "you"
+z = "good ?"
+print(x, y, z)
+
+x = "how  "     #space is required after how
+y = "are  "
+z = " you"
+print(x + y + z)
+
+
+x = 5
+y = 10
+print(x + y)  # Here + is a mathematical operator
+
+x = 2
+y = "hi"
+#print(x + y)   # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+
+if 2>1:
+    print("correct")
+    print("Inside block ")
+print("outside block ")
+#else:
+  #  print("wrong")
+
+
+print("welcome to python ")
