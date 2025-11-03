bc3ca7e72769d61fcfe9ebd6e682ec347c3cbe8e
NDITtech <ndittech1@gmail.com>
2025-11-03 09:31:39 +0530

    OOPS added

--- FUnctions/Basic_func.py
+++ FUnctions/Basic_func.py
@@ -65,3 +65,25 @@ print(p,q,r)
 
 m = math_operations(10,5)
 print(m)
+
+
+
+from abc import ABC, abstractmethod
+class Car(ABC):
+    @abstractmethod
+    def mileage(self):
+        pass
+
+class Tesla(Car):
+    def mileage(self):
+        print("The mileage is 30kmph")
+class Suzuki(Car):
+    def mileage(self):
+        print("The mileage is 25kmph ")
+class Duster(Car):
+     def mileage(self):
+          print("The mileage is 24kmph ")
+
+class Renault(Car):
+    def mileage(self):
+        print("The mileage is 27kmph ")
